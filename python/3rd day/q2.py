49278bdc59cf1ccd8f4be638c674cf0c848d018e
Rajdeep Mishra <137595513+Probityrajdeep@users.noreply.github.com>
2025-12-23 11:41:30 +0530

    22

--- python/3rd day/q2.py
+++ python/3rd day/q2.py
@@ -23,6 +23,4 @@ def cong_solution(a,b,m):
                 break
         print(f"The equation has {d} solution \nand the solution is x = {final_solution} (mod{m}).")
 cong_solution(a,b,m)
-#fsafsafs
-#you are great
-#i love coding
+#11x ≡ 22 (mod 33)
