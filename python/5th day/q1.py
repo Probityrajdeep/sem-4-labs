fd4e4fee3e930fa81f3268a729a4df9a07595e21
Rajdeep Mishra <137595513+Probityrajdeep@users.noreply.github.com>
2026-01-06 10:07:14 +0530

    python

---
+++ python/5th day/q1.py
@@ -0,0 +1,11 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import sympy as sp
+
+print(math.sqrt(16))
+print(math.gcd(55, 33))
+print(np.array([1, 2, 3]))
+
+x = sp.symbols('x')
+print(sp.integrate(x**2, x))
