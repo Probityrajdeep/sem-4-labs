c6624e1d0110878e58fca1751beeff36f77c86c8
Rajdeep Mishra <137595513+Probityrajdeep@users.noreply.github.com>
2026-01-09 11:04:21 +0530

    datasetplot

--- ML/2nd class/create data.py
+++ ML/2nd class/create data.py
@@ -2,14 +2,27 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 data = {
-    "Student_ID": [1,2,3,4,5],
+    "Student_ID": [2,0,8,4,2],
     "Marks": [78,85,62,90,74],
     "Attendance": [80,90,70,95,85]
 }
 df =pd.DataFrame(data)
+#-----------------------------
+# Bar chart
+#-----------------------------
 plt.figure()
 plt.bar(df["Student_ID"],df["Marks"])
 plt.xlabel("Student ID")
 plt.ylabel("Marks")
 plt.title("Bar Chart of Student Marks")
+plt.show()
+#-----------------------------
+#Line Plot
+#-----------------------------
+#line plot
+plt.figure()
+plt.plot(df["Student_ID"],df["Marks"])
+plt.xlabel("Student ID")
+plt.ylabel("Marks")
+plt.title("Line Chart of Student Marks")
 plt.show()
