f048dad3ce0ab8efdf4803607a66051299b4f79f
Rajdeep Mishra <137595513+Probityrajdeep@users.noreply.github.com>
2026-01-09 10:26:37 +0530

    data

---
+++ ML/2nd class/create data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+data = {
+    "Student_ID": [1,2,3,4,5],
+    "Marks": [78,85,62,90,74],
+    "Attendance": [80,90,70,95,85]
+}
+df =pd.DataFrame(data)
+print(data)
