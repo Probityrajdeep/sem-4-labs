26bd1d6c2b5252cbcc9ee41e1f34575c2ae94459
Rajdeep Mishra <137595513+Probityrajdeep@users.noreply.github.com>
2026-01-13 10:42:40 +0530

    updated ni

--- python/6th day/q1.py
+++ python/6th day/q1.py
@@ -2,7 +2,7 @@ import numpy as np
 A = np.array([[1, 1], [1, -1]])
 B = np.array([3, 1])
 
-DET=np.linalg.det(A)
-print("Determinant of A= {int(DET)}")
+det=np.linalg.det(A)
+print("Determinant of A= {int(det)}")
 x,y= np.linalg.solve(A,B)
 print(f"Solution: x={int(x)}, y={int(y)}")
