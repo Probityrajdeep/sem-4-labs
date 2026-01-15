0194ad0d8a7dfd9c296209627c60b2fc1823c69d
Rajdeep Mishra <137595513+Probityrajdeep@users.noreply.github.com>
2026-01-15 11:02:40 +0530

    sort

---
+++ DAA/exp2/quick_sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void quickSort(int a[], int first, int last) {
+    int i, j, pivot, temp;
+    
+    if (first < last) {
+        pivot = first;   
+        i = first;
+        j = last;
+
+        while (i < j) {
+            while (a[i] <= a[pivot] && i < last)
+                i++;
+            while (a[j] > a[pivot])
+                j--;
+            if (i < j) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+
+        temp = a[pivot];
+        a[pivot] = a[j];
+        a[j] = temp;
+
+        quickSort(a, first, j - 1);
+        quickSort(a, j + 1, last);
+    }
+}
+
+int main() {
+    int a[20], n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter the elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    quickSort(a, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
