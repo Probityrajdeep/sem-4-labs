d3164862900bb7034d4be9e5204eabb1088246c6
Rajdeep Mishra <137595513+Probityrajdeep@users.noreply.github.com>
2026-01-20 11:19:15 +0530

    GAUSS_SEIDEL

--- python/7th day/gauss_seidel.py
+++ python/7th day/gauss_seidel.py
@@ -6,6 +6,6 @@ def gauss_sedel(a1,b1,c1,d1, a2,b2,c2,d2, a3,b3,c3,d3):
         z=(d3 - a3*x - b3*y)/c3
     return x,y,z
 
-n=10000
-x_n,y_n,z_n=gauss_sedel(12,3,-5,1 ,2,5,1,1, 2,1,1,3,6)
+n=2
+x_n,y_n,z_n=gauss_sedel(4,-1,0,3, -1,4,-1,7, 0,-1,4,5)
 print("The value of x,y,z are:", x_n, y_n, z_n)
