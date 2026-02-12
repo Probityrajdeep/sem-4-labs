f417ed3297119063a3b8f8b538a175f3e3af29c2
Rajdeep Mishra <137595513+Probityrajdeep@users.noreply.github.com>
2026-02-12 13:09:06 +0530

    hjjh

--- DAA/exp 5/01knapsac.c
+++ DAA/exp 5/01knapsac.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Function to find maximum of two numbers
 int max(int a, int b){
     if(a > b)
         return a;
@@ -10,22 +9,21 @@ int max(int a, int b){
 
 int main(){
 
-    int n = 4;                 // number of items
-    int W = 7;                 // knapsack capacity
+    int n = 4;               
+    int W = 7;          
 
     int weight[] = {1,3,4,5};
     int value[]  = {1,4,5,7};
 
-    int T[10][10];             // DP table
+    int T[10][10];          
 
-    // Base Condition
     for(int i=0;i<=n;i++){
         for(int j=0;j<=W;j++){
 
             if(i==0 || j==0)
                 T[i][j] = 0;
 
-            // Apply Formula
+
             else if(weight[i-1] <= j)
                 T[i][j] = max(T[i-1][j],
                               value[i-1] + T[i-1][j-weight[i-1]]);
